f87b186e6a625a93cd045353990ec7a5c70135f3
alexander <clayderman35@hotmail.com>
2023-04-05 17:39:44 -0300

    empezando a configurar la aplicacion de autentificacion

--- proyectoFinal/settings.py
+++ proyectoFinal/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'contacto',
     'tienda',
     'carro',
+    'autenticacion',
+
 
 
 ]
--- proyectoFinal/urls.py
+++ proyectoFinal/urls.py
@@ -20,8 +20,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('servicios/', include('servicios.urls')),
     path('blog/', include('blog.urls')),
+    path('autenticacion/', include('autenticacion.urls')),
     path('contacto/', include('contacto.urls')),
     path('tienda/', include('tienda.urls')),
+
     path('carro', include('carro.urls')),
     
     path('', include('proyectoWebApp.urls')),
