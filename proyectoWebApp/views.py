722c09288090fba9d36de4e603c3ee5026dee175
alexander <clayderman35@hotmail.com>
2023-04-06 17:36:57 -0300

    proyecto a un paso de terminar

--- proyectoWebApp/views.py
+++ proyectoWebApp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, HttpResponse
+from carro.carro import Carro
 from servicios.models import Servicio
 
 # Create your views here.
 
 def home (request):
+    carro=Carro(request)
     return render(request, "proyectoWebApp/home.html")
 
 def nosotros (request):
